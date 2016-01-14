235d853b535f49f8fce3291be0c8e1e685eed62f
Rich <richard.millings.em@gmail.com>
2016-01-14 14:56:02 +0800

    补充信息

--- GameTest/gulpfile.js
+++ GameTest/gulpfile.js
@@ -4,19 +4,34 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
-gulp.task('build', function () {
+gulp.task('buildJS', function () {
     var tsResult = gulp.src(['src/**/*.ts'])
         .pipe(sourcemaps.init())
         .pipe(ts({
             target: "ES5",
-            //declaration: "true",
-            removeComments: "true",
+            declaration: "true",
+            //removeComments: "true",//去除注释
             module: "commonjs",
             sortOutput: "true",
-            out: "game.js"
+            out: "engine.js"
         }));
     return [
-        tsResult.js.pipe(sourcemaps.write(".")).pipe(gulp.dest('bin-debug')),
-        //tsResult.dts.pipe(gulp.dest('bin-debug'))//生成d.ts文件
+        tsResult.js.pipe(sourcemaps.write(".")).pipe(gulp.dest('bin-debug'))
+    ];
+});
+
+gulp.task('buildDTS', function () {
+    var tsResult = gulp.src(['src/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            target: "ES5",
+            declaration: "true",
+            //removeComments: "true",//去除注释
+            module: "commonjs",
+            sortOutput: "true",
+            out: "engine.js"
+        }));
+    return [
+        tsResult.dts.pipe(gulp.dest('bin-debug'))//发布d.ts文件
     ];
 });
